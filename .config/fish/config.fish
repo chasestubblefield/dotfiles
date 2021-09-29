fa18712c8bcdc35673ffd1a8ef31244f84a09a2a
Chase Stubblefield <chasestubblefield@gmail.com>
2021-09-29 12:12:15 -0700

    google drive, deno, gitconfig

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
 status --is-interactive; and type -q rbenv; and source (rbenv init - | psub)
 status --is-interactive; and type -q nodenv; and source (nodenv init - | psub)
 test -d $HOME/.cargo/bin; and set -p PATH $HOME/.cargo/bin
+test -d $HOME/.deno/bin; and set -p PATH $HOME/.deno/bin
