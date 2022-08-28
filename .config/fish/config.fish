702faf12b6a4666f4f3bbeebd3a3487ed63aee91
Chase Stubblefield <chasestubblefield@gmail.com>
2022-08-28 02:37:20 -0700

    Save dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
-status --is-interactive; and type -q rbenv; and source (rbenv init - | psub)
-status --is-interactive; and type -q nodenv; and source (nodenv init - | psub)
-test -d $HOME/.cargo/bin; and set -p PATH $HOME/.cargo/bin
-test -d $HOME/.deno/bin; and set -p PATH $HOME/.deno/bin
 fish_add_path /usr/local/sbin
+status --is-interactive; and type -q rbenv; and rbenv init - fish | source
+status --is-interactive; and type -q nodenv; and nodenv init - fish | source
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.deno/bin
