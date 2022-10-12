38bc0330460a1dc267f783ac431ede5ad37d09a5
Chase Stubblefield <chasestubblefield@gmail.com>
2022-10-11 21:28:15 -0700

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,2 @@
-fish_add_path /usr/local/sbin
 status --is-interactive; and type -q rbenv; and rbenv init - fish | source
 status --is-interactive; and type -q nodenv; and nodenv init - fish | source
-fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/.deno/bin
