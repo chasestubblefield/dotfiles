b8a0f40f9fbaa8e7ec36d7af762f5f3e313e9a64
Chase Stubblefield <chasestubblefield@gmail.com>
2024-10-05 00:38:15 -0700

    Changes from Oct 2024 reformat

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,5 @@
 status --is-interactive; and type -q rbenv; and rbenv init - fish | source
 status --is-interactive; and type -q nodenv; and nodenv init - fish | source
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
