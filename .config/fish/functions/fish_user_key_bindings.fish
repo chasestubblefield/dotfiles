dd45176a3887ae10c1890017192c244669053e95
Chase Stubblefield <chasestubblefield@gmail.com>
2024-10-06 23:17:17 -0700

    Fix shell keybinds

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,14 @@
 function fish_user_key_bindings
+  # CTRL-K to delete whole line
   bind --user \ck kill-whole-line # CTRL-K
-  bind --user \e\[91\;5u backward-bigword # CTRL-[
-  bind --user \e\[93\;5u forward-bigword # CTRL-]
-  bind --user \e\[127\;5u backward-kill-bigword # CTRL-BACKSPACE
+
+  # CTRL-[ and CTRL-] to move by whitespace-delimited words
+  bind --user \e\[91\;5u backward-bigword
+  bind --user \e\[93\;5u forward-bigword
+
+  # CTRL-BACKSPACE to delete whitespace-delimited word
+  bind --user \e\[127\;5u backward-kill-bigword
+
+  # CTRL-ENTER to accept and execute
+  bind --user \e\[13\;5u accept-autosuggestion execute
 end
